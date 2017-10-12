be0808eb051ec11227291a7628bffd75c858e91a
aleemrazzaqasc <aleem.razzaq.asc@gmail.com>
2017-10-12 15:09:19 +0500

    Framework Added
    Framework Added

---
+++ CryptoAPI.framework/Headers/CryptoAPI.h
@@ -0,0 +1,17 @@
+//
+//  CryptoAPI.h
+//  CryptoAPI
+//
+
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CryptoAPI.
+FOUNDATION_EXPORT double CryptoAPIVersionNumber;
+
+//! Project version string for CryptoAPI.
+FOUNDATION_EXPORT const unsigned char CryptoAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CryptoAPI/PublicHeader.h>
+
+
