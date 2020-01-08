7310a4d90b43fd29619692194020c748c57652e6
aleemrazzaqasc <aleem.razzaq.asc@gmail.com>
2020-01-08 16:02:55 +0500

    Supported latest devices iPhone 11 series and latest iPad series Fixed few bugs

--- CryptoAPI.framework/Headers/CryptoAPI.h
+++ CryptoAPI.framework/Headers/CryptoAPI.h
@@ -2,7 +2,9 @@
 //  CryptoAPI.h
 //  CryptoAPI
 //
-
+//  Created by Ascertia-MAC on 25/07/2017.
+//  Copyright © 2017 Ascertia-MAc. All rights reserved.
+//
 
 #import <UIKit/UIKit.h>
 
@@ -14,4 +16,5 @@ FOUNDATION_EXPORT const unsigned char CryptoAPIVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <CryptoAPI/PublicHeader.h>
 
+//#import <CryptoAPI/>
 
